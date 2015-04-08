f7e11176b7df423602793e7804cc0db79e4d22b4
Maarten Balliauw <maarten@maartenballiauw.be>
2015-04-08 14:49:49 +0200

    Added comments in service PostDeploy

--- src/Ng/Scripts/PostDeploy.ps1
+++ src/Ng/Scripts/PostDeploy.ps1
@@ -5,11 +5,14 @@ $currentDirectory = [string](Get-Location)
 
 # Install services
 # Uses nssm - from http://nssm.cc/download
+
+## Catalog2RegistrationV3
 $installC2R = "nssm install $serviceNameC2R $currentDirectory\Catalog2RegistrationV3.cmd"
 cmd /C $installC2R 
 Set-Service -Name $serviceNameC2R -DisplayName "NuGet - Catalog2RegistrationV3 - $serviceNameC2R" -Description "Runs Catalog2RegistrationV3." -StartupType Automatic
 sc.exe failure $serviceNameC2R reset= 30 actions= restart/5000 
 
+## Catalog2LuceneV3
 $installC2L = "nssm install $serviceNameC2L $currentDirectory\Catalog2LuceneV3.cmd"
 cmd /C $installC2L 
 Set-Service -Name $serviceNameC2L -DisplayName "NuGet - Catalog2LuceneV3 - $serviceNameC2L" -Description "Runs Catalog2LuceneV3." -StartupType Automatic
